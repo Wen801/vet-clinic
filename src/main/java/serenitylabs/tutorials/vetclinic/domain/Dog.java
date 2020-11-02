f21a9316b4888acef79df46b66bd2022c69cc80e
Wen <wen@Wens-MacBook-Pro.local>
2020-11-01 19:42:42 -0700

    Create a dog builder

--- src/main/java/serenitylabs/tutorials/vetclinic/domain/Dog.java
+++ src/main/java/serenitylabs/tutorials/vetclinic/domain/Dog.java
@@ -1,2 +1,53 @@
-package serenitylabs.tutorials.vetclinic.domain;public class Dog {
+package serenitylabs.tutorials.vetclinic.domain;
+
+import java.security.cert.CertPathBuilder;
+import java.time.LocalDateTime;
+
+public class Dog {
+
+    private final String name;
+    private final String breed;
+    private final LocalDateTime dateOfBirth;
+
+    public Dog(String name, String breed, LocalDateTime dateOfBirth) {
+        this.name = name;
+        this.breed = breed;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public LocalDateTime getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    //dog called creates the dog builder;
+    public static DogBuilder called(String name) {
+        return new DogBuilder(name);
+    }
+
+    public static class DogBuilder {
+        private String name;
+        private String breed;
+        private LocalDateTime birthday;
+
+        public DogBuilder (String name){
+            this.name = name;
+        }
+        public DogBuilder ofBreed(String breed) {
+            this.breed = breed;
+            return this;
+        }
+
+        public Dog bornOn(LocalDateTime birthday) {
+            return new Dog(name, breed, birthday);
+        }
+    }
 }
--- src/test/java/serenitylabs/tutorials/vetclinic/domain/WhenCreatingANewDog.java
+++ src/test/java/serenitylabs/tutorials/vetclinic/domain/WhenCreatingANewDog.java
@@ -1,2 +1,23 @@
-package serenitylabs.tutorials.vetclinic.domain;public class WhenCreatingANewDog {
+package serenitylabs.tutorials.vetclinic.domain;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.time.LocalDateTime;
+
+public class WhenCreatingANewDog {
+    @Test
+    public void it_should_have_a_name() throws Exception {
+
+        LocalDateTime birthday = LocalDateTime.now();
+//        Dog fido = new Dog("Fido","Poodle",birthday);
+        Dog fido = Dog.called("Fido")
+                      .ofBreed("Poodle")
+                      .bornOn(birthday);
+
+        Assert.assertEquals("Fido", fido.getName());
+        Assert.assertEquals("Poodle", fido.getBreed());
+        Assert.assertEquals(birthday, fido.getDateOfBirth());
+
+    }
 }
